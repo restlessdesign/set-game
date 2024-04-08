44bd3e63bafaad36e2a0558d69e066c12aeb4b3c
Kevin Sweeney <hello@kevinsweeney.io>
2024-04-08 12:13:11 -0400

    Add select / deselect logic; clean up new game logic

--- set-game/SetGameModel.swift
+++ set-game/SetGameModel.swift
@@ -1,17 +1,35 @@
 import Foundation
 
 struct SetGameModel {
-    private var deck: [SetCard]
-    private(set) var cardsInPlay: [SetCard]
+    let cardsInASet = 3
+    let initialDrawAmount = 12
     
     private let numberOfShapes = [1, 2, 3]
     private let shapeTypes = SetShape.allCases
     private let shadingTypes = SetShading.allCases
     private let colors = SetColor.allCases
     
+    private var standardDeck: [SetCard] = []
+    
+    private(set) var activeDeck: [SetCard] = []
+    private(set) var cardsInPlay: [SetCard] = []
+    private(set) var selectedCards: [SetCard] = []
+    
     init() {
-        deck = []
+        standardDeck = buildDeck()
+        startNewGame()
+    }
+    
+    mutating func startNewGame() -> Void {
+        activeDeck = standardDeck.shuffled()
         cardsInPlay = []
+        selectedCards = []
+        
+        deal(cardsToDraw: initialDrawAmount)
+    }
+    
+    func buildDeck() -> [SetCard] {
+        var deck: [SetCard] = []
         
         for number in numberOfShapes {
             for shapeType in shapeTypes {
@@ -28,17 +46,45 @@ struct SetGameModel {
             }
         }
         
-        deck.shuffle()
-        deal(cardsToDraw: 12)
+        return deck
     }
     
-    mutating func deal(cardsToDraw cardCount: Int = 3) -> Void {
-        for _ in 0..<cardCount {
-            cardsInPlay.append(deck.removeFirst())
+    mutating func deal(cardsToDraw cardCount: Int? = nil) -> Void {
+        var drawAmount = cardCount ?? cardsInASet
+        for _ in 0..<drawAmount {
+            cardsInPlay.append(activeDeck.removeFirst())
         }
     }
     
-    func checkIfSet(_ a: SetCard, _ b: SetCard, _ c: SetCard) -> Bool {
+    mutating func select(card: SetCard) -> Void {
+        if selectedCards.count == cardsInASet {
+            if hasMadeASet(selectedCards[0], selectedCards[1], selectedCards[2]) {
+                for selectedCard in selectedCards {
+                    if let i = cardsInPlay.firstIndex(of: card) {
+                        cardsInPlay.remove(at: i)
+                    }
+                }
+                
+                deal()
+            }
+            else {
+                selectedCards = [card]
+            }
+        }
+        else {
+            selectedCards.append(card)
+        }
+    }
+    
+    mutating func deselect(card: SetCard) -> Void {
+        if selectedCards.count < cardsInASet {
+            if let i = selectedCards.firstIndex(of: card) {
+                selectedCards.remove(at: i)
+            }
+        }
+    }
+    
+    func hasMadeASet(_ a: SetCard, _ b: SetCard, _ c: SetCard) -> Bool {
         // TODO: Break this logic into smaller parts or find a way to generalize
         // They all have the same number or have three different numbers
         return (
