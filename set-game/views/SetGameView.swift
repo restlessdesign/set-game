dc5e2e02b7561cd1339844cb74ee56e62c3662a5
Kevin Sweeney <hello@kevinsweeney.io>
2024-04-08 10:28:39 -0400

    Create initial view structure for game

--- set-game/views/SetGameView.swift
+++ set-game/views/SetGameView.swift
@@ -10,12 +10,46 @@ import SwiftUI
 struct SetGameView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            header
+            Spacer()
+            gameArea
+            Spacer()
+            gameActions
         }
-        .padding()
+    }
+    
+    var header: some View {
+        VStack {
+            Button("New Game", systemImage: "folder.fill.badge.plus") {
+                print("New game")
+            }
+            .buttonStyle(.bordered)
+            .controlSize(.mini)
+            Divider()
+        }
+    }
+    
+    var gameArea: some View {
+        VStack {
+            Text("TODO")
+        }
+    }
+    
+    var gameActions: some View {
+        Group {
+            Button {
+                print("Deal")
+            } label: {
+                Text("Deal")
+                    .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+            }
+            .buttonStyle(.borderedProminent)
+            .font(.title2)
+            .fontWeight(.bold)
+            .controlSize(.large)
+            .disabled(true)
+        }
+        .padding(.horizontal, 50)
     }
 }
 
