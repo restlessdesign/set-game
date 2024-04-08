42499976f894d5c4e9bef9e47e8e2231164c33ed
Kevin Sweeney <hello@kevinsweeney.io>
2024-04-07 21:53:32 -0400

    Initial Commit

---
+++ set-game/set_gameApp.swift
@@ -0,0 +1,17 @@
+//
+//  set_gameApp.swift
+//  set-game
+//
+//  Created by Kevin Sweeney on 4/7/24.
+//
+
+import SwiftUI
+
+@main
+struct set_gameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
